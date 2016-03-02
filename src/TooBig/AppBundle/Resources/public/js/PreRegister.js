fe6943c846b6c350e7e40c10c8eeb842cf6717c3
sergey <malsn@rambler.ru>
2016-03-02 14:04:57 +0300

    registration

--- src/TooBig/AppBundle/Resources/public/js/PreRegister.js
+++ src/TooBig/AppBundle/Resources/public/js/PreRegister.js
@@ -52,8 +52,8 @@ jQuery(document).ready(function() {
                         jQuery('.finish-register-button').click(function(){
                             finish_register();
                         });
-                        jQuery('.pre-register-phone-button').click(function(){
-                            pre_register_phone();
+                        jQuery('.btn-ajax-to-modal').click(function(){
+                            ajax_to_modal(jQuery(this));
                         });
                         if (response.error != '') {
                             $check_code.html(response.error);
