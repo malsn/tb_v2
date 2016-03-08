634e1be90a172b2383ba83551a70d02aa28ffb0a
sergey <malsn@rambler.ru>
2016-03-09 00:41:59 +0300

    auth

--- src/Application/Sonata/UserBundle/Controller/SecurityFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/SecurityFOSUser1Controller.php
@@ -44,7 +44,7 @@ class SecurityFOSUser1Controller extends SecurityController
 
     protected function renderLogin(array $data)
     {
-        $resp_login = $this->loginFormAction();
+        $resp_login = $this->loginFormAction($data);
         $resp_login = preg_replace('/[\r\n]/i','',$resp_login->getContent());
         $this->container->get('flash_bag')->addMessage(
             $resp_login
@@ -56,37 +56,9 @@ class SecurityFOSUser1Controller extends SecurityController
      * @param array $data
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function loginFormAction()
+    public function loginFormAction(array $form_data)
     {
-        $request = $this->container->get('request');
-        /* @var $request \Symfony\Component\HttpFoundation\Request */
-        $session = $request->getSession();
-        /* @var $session \Symfony\Component\HttpFoundation\Session\Session */
-
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = '';
-        }
-
-        if ($error) {
-            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
-            $error = $error->getMessage();
-        }
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
-        $csrfToken = $this->container->has('form.csrf_provider') ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate') : null;
-        $data = array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            'csrf_token' => $csrfToken,
-        );
-
         $template = sprintf('FOSUserBundle:Security:login.html.%s', $this->container->getParameter('fos_user.template.engine'));
-        return $this->container->get('templating')->renderResponse($template, $data);
+        return $this->container->get('templating')->renderResponse($template, $form_data);
     }
 }
