9127b4fc475a08a6441590d104d81161e47b4a3d
sergey <malsn@rambler.ru>
2016-03-09 00:38:00 +0300

    auth

--- src/Application/Sonata/UserBundle/Controller/SecurityFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/SecurityFOSUser1Controller.php
@@ -15,6 +15,7 @@ use FOS\UserBundle\Controller\SecurityController;
 use Sonata\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Security\Core\SecurityContext;
 
 /**
  * Class SecurityFOSUser1Controller.
@@ -57,10 +58,31 @@ class SecurityFOSUser1Controller extends SecurityController
      */
     public function loginFormAction()
     {
+        $request = $this->container->get('request');
+        /* @var $request \Symfony\Component\HttpFoundation\Request */
+        $session = $request->getSession();
+        /* @var $session \Symfony\Component\HttpFoundation\Session\Session */
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
         $csrfToken = $this->container->has('form.csrf_provider') ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate') : null;
         $data = array(
-            'last_username' => '',
-            'error'         => null,
+            'last_username' => $lastUsername,
+            'error'         => $error,
             'csrf_token' => $csrfToken,
         );
 
