08e309a7ef32a4543c1a622ef6632e8c0a9dce5c
sergey <malsn@rambler.ru>
2016-03-09 18:32:34 +0300

    auth for user section

--- src/TooBig/AppBundle/Controller/UserController.php
+++ src/TooBig/AppBundle/Controller/UserController.php
@@ -38,14 +38,21 @@ class UserController extends RubricAwareController
      */
     public function showAction()
     {
+
         $user = $this->get('security.context')->getToken()->getUser();
+
         if (!is_object($user) || !$user instanceof UserInterface) {
-            throw $this->createAccessDeniedException('This user does not have access to this section.');
+
+            return $this->getUserLoginForm();
+
+        } else {
+
+            return $this->render('TooBigAppBundle:User:edit_user_profile.html.twig', array(
+                'user'   => $user, 'breadcrumbs' => []
+            ));
+
         }
 
-        return $this->render('TooBigAppBundle:User:edit_user_profile.html.twig', array(
-            'user'   => $user, 'breadcrumbs' => []
-        ));
     }
 
     /**
@@ -53,14 +60,24 @@ class UserController extends RubricAwareController
      */
     public function watchlistAction (){
 
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        $repository = $this->getDoctrine()->getRepository('TooBigAppBundle:ItemSubscribtion');
-        $query = $repository->createQueryBuilder('p')
-            ->where('p.user = :user')
-            ->orderBy('p.updatedAt', 'DESC')
-            ->setParameter('user', $user->getId());
+        $user = $this->get('security.context')->getToken()->getUser();
 
-        return array('entities' => $this->paginate($query, 20));
+        if (!is_object($user) || !$user instanceof UserInterface) {
+
+            return $this->getUserLoginForm();
+
+        } else {
+
+            $repository = $this->getDoctrine()->getRepository('TooBigAppBundle:ItemSubscribtion');
+            $query = $repository->createQueryBuilder('p')
+                ->where('p.user = :user')
+                ->orderBy('p.updatedAt', 'DESC')
+                ->setParameter('user', $user->getId());
+
+            return array('entities' => $this->paginate($query, 20));
+
+        }
+
     }
 
     /**
@@ -68,12 +85,21 @@ class UserController extends RubricAwareController
      */
     public function listUserItemsAction (){
 
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        $query = $this->getDoctrine()->getRepository('TooBigAppBundle:Item')->createQuery('c', function ($qb) use ($user)
-        {
-            $qb->whereCreatedBy($user);
-        });
-        return array('entities' => $this->paginate($query, 20));
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        if (!is_object($user) || !$user instanceof UserInterface) {
+
+            return $this->getUserLoginForm();
+
+        } else {
+
+            $query = $this->getDoctrine()->getRepository('TooBigAppBundle:Item')->createQuery('c', function ($qb) use ($user)
+            {
+                $qb->whereCreatedBy($user);
+            });
+            return array('entities' => $this->paginate($query, 20));
+
+        }
     }
 
     /**
@@ -81,16 +107,26 @@ class UserController extends RubricAwareController
      */
     public function listSubscriptionsAction (){
 
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        $query = $this->getDoctrine()
-            ->getRepository('TooBigAppBundle:AutoSubscription')
-            ->createQueryBuilder('s')
-            ->where('s.createdBy = :user')
-            ->setParameter('user',$user->getId())
-            ->orderBy('s.createdAt','DESC')
-            ->getQuery();
+        $user = $this->get('security.context')->getToken()->getUser();
 
-        return array('entities' => $this->paginate($query, 20));
+        if (!is_object($user) || !$user instanceof UserInterface) {
+
+            return $this->getUserLoginForm();
+
+        } else {
+
+            $query = $this->getDoctrine()
+                ->getRepository('TooBigAppBundle:AutoSubscription')
+                ->createQueryBuilder('s')
+                ->where('s.createdBy = :user')
+                ->setParameter('user',$user->getId())
+                ->orderBy('s.createdAt','DESC')
+                ->getQuery();
+
+            return array('entities' => $this->paginate($query, 20));
+
+        }
+
     }
 
     /**
@@ -101,42 +137,49 @@ class UserController extends RubricAwareController
     public function editProfileAction(Request $request)
     {
         $user = $this->get('security.context')->getToken()->getUser();
+
         if (!is_object($user) || !$user instanceof UserInterface) {
-            throw $this->createAccessDeniedException('This user does not have access to this section.');
-        }
 
-        $form = $this->createForm(
-            new ProfileType('FOS\UserBundle\Entity\User'),
-            $user
-        );
+            return $this->getUserLoginForm();
 
-        if ($request->isMethod('POST')) {
-            $form->handleRequest($request);
-            if ($form->isValid()) {
-                try {
-                    $this->get('fos_user.user_manager')->updateUser($user);
-                    $this->get('fos_user.user_manager')->reloadUser($user);
-                    $this->get('flash_bag')->addMessage('Ваши данные успешно обновлены!');
-                } catch (\Exception $e) {
-                    $this->get('flash_bag')->addMessage('Произошла ошибка при обновлении данных!'.$e->getMessage().$form->getErrors()->next());
+        } else {
+
+            $form = $this->createForm(
+                new ProfileType('FOS\UserBundle\Entity\User'),
+                $user
+            );
+
+            if ($request->isMethod('POST')) {
+                $form->handleRequest($request);
+                if ($form->isValid()) {
+                    try {
+                        $this->get('fos_user.user_manager')->updateUser($user);
+                        $this->get('fos_user.user_manager')->reloadUser($user);
+                        $this->get('flash_bag')->addMessage('Ваши данные успешно обновлены!');
+                    } catch (\Exception $e) {
+                        $this->get('flash_bag')->addMessage('Произошла ошибка при обновлении данных!'.$e->getMessage().$form->getErrors()->next());
+                    }
+                } else {
+                    $this->get('flash_bag')->addMessage('Произошла ошибка при обновлении данных!'.$form->getErrors()->next());
                 }
-            } else {
-                $this->get('flash_bag')->addMessage('Произошла ошибка при обновлении данных!'.$form->getErrors()->next());
             }
-        }
 
 
             return $this->render('TooBigAppBundle:User:edit_user_profile.html.twig', array(
-            'form' => $form->createView()
-        ));
+                'form' => $form->createView()
+            ));
+
+        }
+
     }
 
-    /**
-     * @param string $action
-     * @param string $value
-     */
-    protected function setFlash($action, $value)
-    {
-        $this->get('session')->getFlashBag()->set($action, $value);
+    protected function getUserLoginForm(){
+        $resp_login = $this->forward('ApplicationSonataUserBundle:SecurityFOSUser1:loginForm');
+        $resp_login = preg_replace('/[\r\n]/i','',$resp_login->getContent());
+        $_SESSION['return_url'] = $this->get('request_stack')->getMasterRequest()->server->get('REQUEST_URI');
+        $this->get('flash_bag')->addMessage(
+            $resp_login
+        );
+        return $this->forward('TooBigAppBundle:Item:siteIndex');
     }
 }
