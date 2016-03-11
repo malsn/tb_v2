6be0f8f368265df0e073bcfeaf9f4f48192abc5e
sergey <malsn@rambler.ru>
2016-03-11 23:21:22 +0300

    colorpicker and kernel

--- src/TooBig/AppBundle/Form/Type/ItemEditForm.php
+++ src/TooBig/AppBundle/Form/Type/ItemEditForm.php
@@ -12,6 +12,7 @@ use Iphp\CoreBundle\Admin\Admin;
 use Application\Iphp\ContentBundle\Entity\Content;
 use Application\Iphp\CoreBundle\Entity\Rubric;
 use TooBig\AppBundle\Entity\Item;
+use Oh\ColorPickerTypeBundle\Form\Type\ColorPickerType;
 
 
 class ItemEditForm extends AbstractType
@@ -60,7 +61,7 @@ class ItemEditForm extends AbstractType
             /*->add('rubric', 'rubricchoice')*/
             ->add('gender', new GenderType(), ['empty_value' => 'Укажите пол'])
             ->add('status', new StatusType(), ['empty_value' => 'Укажите состояние'])
-            ->add('color')
+            ->add('color', new ColorPickerType())
             /*->add('brand', 'entity', [
                 'class'=>'TooBig\AppBundle\Entity\Brand',
                 'empty_value' => 'Укажите бренд',
