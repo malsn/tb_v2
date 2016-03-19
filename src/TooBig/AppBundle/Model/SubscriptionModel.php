5cd12a2701b35eb7b04c5deedb9febf2384ccdc5
sergey <malsn@rambler.ru>
2016-03-19 19:24:18 +0300

    Item subscription email

--- src/TooBig/AppBundle/Model/SubscriptionModel.php
+++ src/TooBig/AppBundle/Model/SubscriptionModel.php
@@ -117,7 +117,10 @@ class SubscriptionModel extends ContainerAware {
                         } else {
                             switch($key){
                                 case 'Color':
-                                    $qb->andWhere($qb->expr()->in('c.color_id',$value->getSnapshot()));
+                                    $qb->andWhere($qb->expr()->in('c.color',array_map(
+                                        function ($collection) {
+                                            return $collection->getId();
+                                        }, $value->getSnapshot())));
                                 default:
                             }
                         }
