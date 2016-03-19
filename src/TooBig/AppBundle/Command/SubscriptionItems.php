4f63e2db16d6f361104e08f4bb59e1bddb2c90c3
sergey <malsn@rambler.ru>
2016-03-19 16:27:10 +0300

    Item subscription email

--- src/TooBig/AppBundle/Command/SubscriptionItems.php
+++ src/TooBig/AppBundle/Command/SubscriptionItems.php
@@ -32,30 +32,7 @@ class SubscriptionItems extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $subscriptions = $this->getContainer()->get('doctrine')->getRepository('TooBigAppBundle:AutoSubscription')->findAll();
-        foreach ( $subscriptions as $subscription){
-            $user = $subscription->getCreatedBy();
-            $query = $this->getContainer()->get('auto_subscription_model')->getItemsBySubscriptionQuery($subscription);
-            $count = count($query->getResult());
-            if ($count > 0) {
-                // получаем 'mailer' (обязателен для инициализации Swift Mailer)
-                $mailer = $this->getContainer()->get('mailer');
-                $message = \Swift_Message::newInstance()
-                    ->setSubject('Новые предложения по вашей подписке на площадке TooBig')
-                    ->setFrom('admin@old-stuff.spbeta.ru')
-                    ->setTo($user->getEmail())
-                    ->setContentType('text/html')
-                    ->setBody(sprintf("Уважаемый(ая), %s. По вашей подписке \"%s\" для вас появилось %d новых объявлений. Чтобы их посмотреть, перейдите в раздел <a href='%s'>Мои подписки</a>.",
-                        $user->getFirstName(),
-                        $subscription->getTitle(),
-                        $count,
-                        $this->getContainer()->get('router')->generate('app_list_subscriptions'))
-                    );
-                $mailer->send($message);
-                /**/
-                $output->writeln($subscription->getId());
-            }
-        }
+        
 
     }
 }
