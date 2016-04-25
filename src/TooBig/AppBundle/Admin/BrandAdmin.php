ce823ebd9f5640c53a749a62f7f9ad8c0cf77634
sergey <malsn@rambler.ru>
2016-04-25 11:23:11 +0300

    brand

--- src/TooBig/AppBundle/Admin/BrandAdmin.php
+++ src/TooBig/AppBundle/Admin/BrandAdmin.php
@@ -15,7 +15,7 @@ class BrandAdmin extends Admin
             ->add('name', 'text', array('label' => 'Наименование'))
             ->add('description', null, array('label' => 'Описание')) //if no type is specified, SonataAdminBundle tries to guess it
             ->add('imageUpload', 'file', ['required' => false])
-            //->add('image', 'iphp_file',['upload' => false])
+            ->add('image', 'iphp_file',['upload' => false])
         ;
     }
 
