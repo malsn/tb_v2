06f3ac0e38dd5106ab8d61380796cec8c990f9f6
sergey <malsn@rambler.ru>
2016-07-03 22:28:10 +0300

    size controller

--- src/TooBig/AppBundle/Controller/Admin/AdminSizeController.php
+++ src/TooBig/AppBundle/Controller/Admin/AdminSizeController.php
@@ -16,14 +16,15 @@ class AdminSizeController extends Controller
     public function listSizebySizeTypeAction( Request $request ){
         $keys = $request->request->keys();
         $size_type = $this->get('sizetype_model')->getSizeTypeById( $request->request->get($keys[1])['size_type'] );
+        $size_country = $this->get('sizecountry_model')->getSizeCountryById( $request->request->get($keys[1])['size_country'] );
         $form_array = $request->request->get($keys[1]);
         if (array_key_exists('size',$form_array)) {
             $item_size = $form_array['size'];
         } else {
             $item_size = null;
         }
-        if (!is_null($size_type)) {
-            $sizes = $this->get('size_model')->getSizeBySizeType($size_type);
+        if ( null !== $size_type && null !== $size_country ) {
+            $sizes = $this->get('size_model')->getSizeBySizeType($size_type, $size_country);
             return $this->render('TooBigAppBundle:Admin\Size:size_by_sizetype.html.twig', [
                 'sizes' => $sizes,
                 'form_name' => $keys[1],
