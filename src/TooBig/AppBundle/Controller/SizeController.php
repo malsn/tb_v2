27b8abb72e011e540c55f5a51587ed25cdf49cd6
sergey <malsn@rambler.ru>
2016-07-03 22:24:10 +0300

    test size controller

--- src/TooBig/AppBundle/Controller/SizeController.php
+++ src/TooBig/AppBundle/Controller/SizeController.php
@@ -16,8 +16,9 @@ class SizeController extends Controller
     public function listSizebySizeTypeAction( Request $request ){
         $keys = $request->request->keys();
         $size_type = $this->get('sizetype_model')->getSizeTypeById( $request->request->get($keys[0])['size_type'] );
-        $size_country = $this->get('sizecountry_model')->getSizeCountryById( $_POST['size_country'] );
-        $form_array = $request->request->get($keys[0]);
+        $size_country = $this->get('sizecountry_model')->getSizeCountryById( $request->request->get($keys[0])['size_country'] );
+        return $request->request->get($keys[0]);
+        /*$form_array = $request->request->get($keys[0]);
         if (array_key_exists('size',$form_array)) {
             $item_size = $form_array['size'];
         } else {
@@ -32,6 +33,6 @@ class SizeController extends Controller
             ]);
         } else {
             return false;
-        }
+        }*/
     }
 }
