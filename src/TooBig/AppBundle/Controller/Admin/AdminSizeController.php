e926123115a50c95a0788f0c2512dfb7d555256d
sergey <malsn@rambler.ru>
2016-07-04 21:40:55 +0300

    item rubric sizetype

--- src/TooBig/AppBundle/Controller/Admin/AdminSizeController.php
+++ src/TooBig/AppBundle/Controller/Admin/AdminSizeController.php
@@ -34,4 +34,29 @@ class AdminSizeController extends Controller
             return false;
         }
     }
+
+    /**
+     * @Route("/admin/size/type/{$num}", name="admin_list_size_by_sizenumtype")
+     */
+    public function listSizebySizeNumTypeAction( Request $request, $num ){
+        $keys = $request->request->keys();
+        $size_type = $this->get('sizetype_model')->getSizeTypeById( $request->request->get($keys[1])['size_type_'.$num] );
+        $size_country = $this->get('sizecountry_model')->getSizeCountryById( $request->request->get($keys[1])['size_country_'.$num] );
+        $form_array = $request->request->get($keys[1]);
+        if (array_key_exists('size_'.$num,$form_array)) {
+            $item_size = $form_array['size_'.$num];
+        } else {
+            $item_size = null;
+        }
+        if ( null !== $size_type && null !== $size_country ) {
+            $sizes = $this->get('size_model')->getSizeBySizeType($size_type, $size_country);
+            return $this->render('TooBigAppBundle:Admin\Size:size_by_sizetype.html.twig', [
+                'sizes' => $sizes,
+                'form_name' => $keys[1],
+                'item_size' => $item_size
+            ]);
+        } else {
+            return false;
+        }
+    }
 }
