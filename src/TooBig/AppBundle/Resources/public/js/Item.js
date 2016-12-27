500de430c9507681af164db624eb05ca76cb22b0
sergey <malsn@rambler.ru>
2016-12-28 02:23:39 +0300

    color-box-filter

--- src/TooBig/AppBundle/Resources/public/js/Item.js
+++ src/TooBig/AppBundle/Resources/public/js/Item.js
@@ -171,7 +171,7 @@ jQuery(document).ready(function() {
                 .siblings("input[type=radio]")
                 .attr("checked",false);
             jQuery(this).toggleClass('glyphicon glyphicon-ok');
-            sibcheckbox.attr("checked", !sibcheckbox.attr("checked"));
+            sibcheckbox.attr("checked") ? sibcheckbox.attr("checked", false) : sibcheckbox.attr("checked", true);
         });
 
     jQuery(".glyphicon-remove.filter-result-params")
